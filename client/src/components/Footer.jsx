3f62bcb74e6a12b31afb93ab971738c13d6fa616
priya281220 <144831117+priya281220@users.noreply.github.com>
2024-05-21 21:45:17 +0530

    Update Footer.jsx

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,5 +1,6 @@
 import { Footer } from 'flowbite-react';
 import { Link } from 'react-router-dom';
+import my_image from './my_image.jpg';
 import { BsFacebook, BsInstagram, BsTwitter, BsGithub, BsDribbble, BsLinkedin } from 'react-icons/bs';
 export default function FooterCom() {
   return (
@@ -60,6 +61,14 @@ export default function FooterCom() {
           </div>
         </div>
         <Footer.Divider />
+               <div style={{ position: 'fixed', top: '70px', left: '5px', display: 'flex', alignItems: 'center' }}>
+  <div style={{ position: 'absolute', top: '5px', left: '5px', width: '80px', height: '80px', borderRadius: '50%', overflow: 'hidden', border: '3px solid white' }}>
+    <img src={my_image} alt="Signature" style={{ width: '100%', height: '100%', objectFit: 'cover' }} />
+  </div>
+  <div style={{ position: 'absolute', top: '86px', left: '5px', textAlign: 'center', color: 'white', backgroundColor: 'black', padding: '5px', border: '3px solid white', textShadow: '1px 1px 3px yellow' }}>
+    Created by <span style={{ fontWeight: 'bold' }}>Priya Choudhary</span>
+  </div>
+</div>
         <div className='w-full sm:flex sm:items-center sm:justify-between'>
           <Footer.Copyright
             href='#'
