0e26e1d8fc30f5db677e4ea9789a3c44143ab2bd
priya281220 <144831117+priya281220@users.noreply.github.com>
2025-04-30 22:52:29 +0530

    Update Footer.jsx

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -71,7 +71,7 @@ export default function FooterCom() {
         <div className='w-full sm:flex sm:items-center sm:justify-between'>
           <Footer.Copyright
             href='#'
-            by="Priya's blog"
+            by="Blog"
             year={new Date().getFullYear()}
           />
           <div className="flex gap-6 sm:mt-0 mt-4 sm:justify-center">
